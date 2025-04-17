198924e7edd61f95708b4fa95aa4516079fd1705
linshaoqin <linshaoqin@bytedance.com>
2025-04-17 18:39:55 +0800

    feat: add [by lsq], update deploy

--- composeApp/src/desktopMain/kotlin/com/lsq/adaption/multiplatfrom/main.kt
+++ composeApp/src/desktopMain/kotlin/com/lsq/adaption/multiplatfrom/main.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.window.application
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "adaption_m",
+        title = "adaption_m by lsq",
     ) {
         App()
     }
